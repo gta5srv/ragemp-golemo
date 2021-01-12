d106fe8d293305934b42226a67357ffe3da3fdb5
6er1k <maxborisow9@gmail.com>
2021-01-12 15:34:39 +0300

    Small Fixes(Убрал мусор)

--- server/client_packages/libs/utils/discord.js
+++ server/client_packages/libs/utils/discord.js
@@ -1,4 +1,4 @@
 setInterval(function () {
     var name = (localplayer.getVariable('REMOTE_ID') == undefined) ? `Не авторизован` : `Игрок №${localplayer.getVariable("REMOTE_ID")}`;
-	mp.discord.update('Beyond', name);
+	mp.discord.update('SERVER NAME', name);
 }, 10000);
