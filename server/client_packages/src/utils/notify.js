314bf69cc6103566d1745492f68012d7ea45978c
JJiGolem <lorsanov2000@gmail.com>
2021-01-23 17:50:36 +0300

    [GM-49] Добавлено уведомление "Нажмите Е" на все виды колшейпов для взаимодействия

--- server/client_packages/src/utils/notify.js
+++ server/client_packages/src/utils/notify.js
@@ -5,3 +5,14 @@ mp.events.add('notify', (type, layout, msg, time) => {
     else mp.events.call('authNotify', type, layout, msg, time)
 });
 
+global.INTERACTIONCHECK = false;
+mp.events.add('playerInteractionCheck', function (state){
+    global.INTERACTIONCHECK = state;
+});
+mp.events.add('render', () => {
+    if (global.INTERACTIONCHECK === true) {
+        mp.game.ui.setTextComponentFormat('STRING');
+        mp.game.ui.addTextComponentSubstringPlayerName("~h~Нажмите ~g~ ~INPUT_CONTEXT~ ~s~ для взаимодействия");
+        mp.game.ui.displayHelpTextFromStringLabel((0), false, true, -1);
+    }
+});
