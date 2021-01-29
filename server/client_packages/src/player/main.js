e8824d9f781f6c563a5366db6e68fc1f39b29c2d
JJiGolem <lorsanov2000@gmail.com>
2021-01-29 22:34:01 +0300

    [GM-71] Теперь нельзя брать телефон в руки, если вы упали на землю, а также если находитесь в состоянии падения

--- server/client_packages/src/player/main.js
+++ server/client_packages/src/player/main.js
@@ -221,7 +221,7 @@ mp.keys.bind(Keys.VK_M, false, function () {
         mp.events.callRemote("closePlayerMenu");
         global.phoneOpen = 0;
     }
-    else if (!localplayer.getVariable('AntiAnimDown') && !localplayer.isInWater() && !localplayer.isInAir())
+    else if (!localplayer.getVariable('AntiAnimDown') && !localplayer.isInWater() && !localplayer.isInAir() && !localplayer.isRunningRagdollTask())
     {
         mp.events.callRemote('openPlayerMenu');
         lastCheck = new Date().getTime();
