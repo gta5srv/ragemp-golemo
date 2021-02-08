0090109f6ad624345389faec7b445adec9820dd7
JJiGolem <lorsanov2000@gmail.com>
2021-02-08 04:37:30 +0300

    [GM-95] Добавил восстановление анимации в наручниках, после того, как игрока достают из машины

--- server/client_packages/src/player/playertasks.js
+++ server/client_packages/src/player/playertasks.js
@@ -1,8 +1,15 @@
 mp.events.add('PLAYER::TASK_LEAVE_ON_VEHICLE', () => {
-    if(localplayer.vehicle != null) {
-        let vehicle = localplayer.vehicle;
+    let vehicle = localplayer.vehicle;
+    if(vehicle != null) {
         localplayer.taskLeaveVehicle(vehicle.handle, 1);
     }
+    //todo проверить
+    if(cuffed){
+		while(localplayer.vehicle){
+			mp.game.wait(100);
+		}
+		mp.events.callRemote("cuffUpdate");
+	}
 });
 
 //vehicle.handle
