682cb1030ac3d244bba9aa2198587109477a80ad
JJiGolem <lorsanov2000@gmail.com>
2021-02-14 16:31:07 +0300

    [GM-100] Исправил уведомление, плюс заменил на правильное изображение

--- server/client_packages/browser/assets/js/notify.js
+++ server/client_packages/browser/assets/js/notify.js
@@ -1,7 +1,7 @@
 function notify(type, layout, message, time) {
     var types = ['alert', 'error', 'success', 'information', 'warning'];
     var layouts = ['top', 'topLeft', 'topCenter', 'topRight', 'center', 'centerLeft', 'centerRight', 'bottom', 'bottomLeft', 'bottomCenter', 'bottomRight'];
-    var icons = ['<i class=""><div class="border"></div><img src="./assets/images/icons/notification/error.png" width="25px">Ошибка! </i></i>', '<i class=""><div class="border"></div><img src="./assets/images/icons/notification/error.png" width="25px">Ошибка! </i>', '<i class=""><div class="border"></div><img src="./assets/images/icons/notification/success.png" width="25px">Успешно! </i></i>', '<i class=""><div class="border"></div><img src="./assets/images/icons/notification/info.png" width="25px">Информация! </i>', '<i class=""><div class="border"></div><img src="./assets/images/icons/notification/warning.png" width="25px" >Предупреждение! </i>']
+    var icons = ['<i class=""><div class="border"></div><img src="./assets/images/icons/notification/info.png" width="25px">Уведомление! </i></i>', '<i class=""><div class="border"></div><img src="./assets/images/icons/notification/error.png" width="25px">Ошибка! </i>', '<i class=""><div class="border"></div><img src="./assets/images/icons/notification/success.png" width="25px">Успешно! </i></i>', '<i class=""><div class="border"></div><img src="./assets/images/icons/notification/info.png" width="25px">Информация! </i>', '<i class=""><div class="border"></div><img src="./assets/images/icons/notification/warning.png" width="25px" >Предупреждение! </i>']
     message = '<div class="text">'+icons[type]+message+'</div>';
     new Noty({
         type: types[type],
