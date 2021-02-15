80011c65a0edc189eaa2d5aa8b1837dfe946cb2b
JJiGolem <lorsanov2000@gmail.com>
2021-02-15 19:52:12 +0300

    [F] Убрал непродуманный код

--- server/dotnet/resources/GameMode/Golemo/Core/Inventory.cs
+++ server/dotnet/resources/GameMode/Golemo/Core/Inventory.cs
@@ -1300,13 +1300,6 @@ namespace Golemo.Core
                         }
 
                         var wHash = Weapons.GetHash(item.Type.ToString());
-                        int wInt = 0;
-                        if (!Int32.TryParse(item.Data, out wInt) && Main.Players[player].AdminLVL < 1)
-                        {
-                            nInventory.Remove(player, item.Type, 1);
-                            Notify.Send(player, NotifyType.Error, NotifyPosition.BottomCenter, "Вы не можете использовать админское оружие", 3000);
-                            return;
-                        }
                         if (Weapons.WeaponsAmmoTypes.ContainsKey(item.Type))
                         {
                             var ammoItem = nInventory.Find(UUID, Weapons.WeaponsAmmoTypes[item.Type]);
