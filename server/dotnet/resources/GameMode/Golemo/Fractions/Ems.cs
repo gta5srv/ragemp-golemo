722e7f4ee1a0953a3a51ece85b0523c2235b8ecb
JJiGolem <lorsanov2000@gmail.com>
2021-03-01 04:24:31 +0300

    [GM-155] Теперь если у игрока открыт телефон и умрет, то телефон закроется автоматически как и положено

--- server/dotnet/resources/GameMode/Golemo/Fractions/Ems.cs
+++ server/dotnet/resources/GameMode/Golemo/Fractions/Ems.cs
@@ -289,6 +289,7 @@ namespace Golemo.Fractions
                 Jobs.Gopostal.Event_PlayerDeath(player, entityKiller, weapon);
 
                 if (player.HasData("job_farmer")) Jobs.FarmerJob.Farmer.StartWork(player, false);
+                if (player.HasData("Phone")) MenuManager.Close(player);
 
                 //VehicleManager.WarpPlayerOutOfVehicle(player);
                 Main.Players[player].IsAlive = false;
