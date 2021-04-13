c24f7b93d451f98855ca32baf171ea54580926c9
JJiGolem <lorsanov2000@gmail.com>
2021-04-13 14:59:44 +0300

    [GM-238] Подправил рандомный выбор следующего дома для точки доставки посылки на работе Почтальона
    - Дело в том, что Random().Next() максимальное значение в период рандома не включает.

--- server/dotnet/resources/GameMode/Golemo/Jobs/WorkManager.cs
+++ server/dotnet/resources/GameMode/Golemo/Jobs/WorkManager.cs
@@ -363,9 +363,10 @@ namespace Golemo.Jobs
 
                         client.SetData("W_LASTPOS", client.Position);
                         client.SetData("W_LASTTIME", DateTime.Now);
-                        var next = Jobs.WorkManager.rnd.Next(0, Houses.HouseManager.Houses.Count - 1);
+                        var next = Jobs.WorkManager.rnd.Next(0, Houses.HouseManager.Houses.Count);
                         while (Houses.HouseManager.Houses[next].Position.DistanceTo2D(client.Position) < 200)
-                            next = Jobs.WorkManager.rnd.Next(0, Houses.HouseManager.Houses.Count - 1);
+                            next = Jobs.WorkManager.rnd.Next(0, Houses.HouseManager.Houses.Count);
+
                         client.SetData("NEXTHOUSE", Houses.HouseManager.Houses[next].ID);
 
                         Trigger.ClientEvent(client, "createCheckpoint", 1, 1, Houses.HouseManager.Houses[next].Position, 1, 0, 255, 0, 0);
