209f971c8c9ce5e91c9256a9172ba96aad9e7948
JJiGolem <lorsanov2000@gmail.com>
2021-04-22 11:48:21 +0300

    [GM-247] Добавление возможности двигать уведомление "Нажмите Е" по экрану

--- server/client_packages/src/utils/notify.js
+++ server/client_packages/src/utils/notify.js
@@ -11,6 +11,8 @@ mp.events.add('playerInteractionCheck', function (state){
 });
 mp.events.add('render', () => {
     if (global.INTERACTIONCHECK === true && !global.menuOpened) {
+        mp.game.ui.resetHudComponentValues(10);
+        mp.game.ui.setHudComponentPosition(10, 0, 0); //меняйте эти два нуля. Точно не помню, но первое это по вертикали, второе по горизонтали. от 0 до 1 пробуйте перемещать
         mp.game.ui.setTextComponentFormat('STRING');
         mp.game.ui.addTextComponentSubstringPlayerName("~h~Нажмите ~g~ ~INPUT_CONTEXT~ ~s~ для взаимодействия");
         mp.game.ui.displayHelpTextFromStringLabel((0), false, true, -1);
